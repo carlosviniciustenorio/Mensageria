8ab92e4b43237c12930818ef52fea77dca4a4b9c
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-07-10 16:39:52 -0300

    receive

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost"};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("autorizacoes", false, false, false, null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+        Console.WriteLine(" [x] Received {0}", message);
+    };
+
+    channel.BasicConsume("autorizacoes", true, consumer);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
