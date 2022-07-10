59ebaa9e9ecb063013372556d3d2669b46943c44
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-07-10 18:37:14 -0300

    update

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -2,21 +2,27 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading;
 
 var factory = new ConnectionFactory() { HostName = "localhost"};
 using (var connection = factory.CreateConnection())
 using (var channel = connection.CreateModel())
 {
-    channel.QueueDeclare("autorizacoes", false, false, false, null);
+    channel.QueueDeclare("work", false, false, false, null);
 
     var consumer = new EventingBasicConsumer(channel);
     consumer.Received += (model, ea) =>
     {
         var message = Encoding.UTF8.GetString(ea.Body.ToArray());
         Console.WriteLine(" [x] Received {0}", message);
+        
+        int dots = message.Split('.').Length - 1;
+        Thread.Sleep(dots * 1000);
+
+        Console.WriteLine(" [x] Done");
     };
 
-    channel.BasicConsume("autorizacoes", true, consumer);
+    channel.BasicConsume("work", true, consumer);
     Console.WriteLine(" Press [enter] to exit.");
     Console.ReadLine();
 }
