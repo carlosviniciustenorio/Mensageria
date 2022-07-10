c6034ce94698db433a94ba065b419fd9bf614dc3
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-07-10 15:55:55 -0300

    first commit

---
+++ Send/Send.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("autorizacoes",false,false,false, null);
+    string message = "O usuário Isabelly Ribeiro Tenorio solicita a autorização do usuário Carlos Vinícius Tenorio";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "", routingKey: "autorizacoes", basicProperties: null, body: body);
+    Console.WriteLine("[X] Sent {0}", message);
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
