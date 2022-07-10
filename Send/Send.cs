f70d51eb020b1789313705fd5f30b74ff41a78af
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-07-10 18:50:04 -0300

    duas aplicações consumindo a mesma fila para simular thread

--- Send/Send.cs
+++ Send/Send.cs
@@ -2,20 +2,19 @@
 using RabbitMQ.Client;
 using System.Text;
 
-static void Main(string[] args)
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
 {
-    var factory = new ConnectionFactory() { HostName = "localhost" };
-    using (var connection = factory.CreateConnection())
-    using (var channel = connection.CreateModel())
-    {
-        channel.QueueDeclare("work", false, false, false, null);
-        string message = GetMessage(args);
+    string[] arg = { "First message.", "Second message..", "Third message...", "Fourth message....", "Fifth message....." };
+    channel.QueueDeclare("work", false, false, false, null);
+    string message = GetMessage(arg);
 
-        var body = Encoding.UTF8.GetBytes(message);
+    var body = Encoding.UTF8.GetBytes(message);
 
-        channel.BasicPublish(exchange: "", routingKey: "work", basicProperties: null, body: body);
-        Console.WriteLine("[X] Sent {0}", message);
-    }
+    channel.BasicPublish(exchange: "", routingKey: "work", basicProperties: null, body: body);
+    Console.WriteLine("[X] Sent {0}", message);
 }
 
 static string GetMessage(string[] args)
