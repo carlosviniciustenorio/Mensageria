ebc1449cedc1cce44db28855ff34af92dfc38215
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-09-07 12:02:12 -0300

    .

--- SNS/Program.cs
+++ SNS/Program.cs
@@ -5,24 +5,27 @@ public class Program
 {
     static async Task Main(string[] args)
     {
-        string message = "Teste Amazon SNS";
+        string message = "De todo nosso coração, estamos convidando você para ser nossa madrinha de casamento. Aceita? <3 -- Carlos & Isabelly";
 
-        var client = new AmazonSimpleNotificationServiceClient(awsAccessKeyId: "",
-                                                               awsSecretAccessKey: "",
+        var client = new AmazonSimpleNotificationServiceClient(awsAccessKeyId: "AKIA4HZNBE7FYRJQYNPG",
+                                                               awsSecretAccessKey: "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",
                                                                region: Amazon.RegionEndpoint.SAEast1);
 
-        var request = new PublishRequest { Message = message, TopicArn = ""};
+        var request = new PublishRequest { Message = message, TopicArn = "arn:aws:sns:sa-east-1:841371166667:SMSTopic" };
 
-        try
+        for (int i = 0; i < 10; i++)
         {
-            var response = client.PublishAsync(request);
-            Console.WriteLine(response);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Caught exception publishing request:");
-            Console.WriteLine(ex.Message);
-            throw;
+            try
+            {
+                var response = client.PublishAsync(request);
+                Console.WriteLine(response);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Caught exception publishing request:");
+                Console.WriteLine(ex.Message);
+                throw;
+            }
         }
     }
 }
